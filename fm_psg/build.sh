f7dffc518108871e7bbeb2caf825554cc2b024c8
mmlbox <mmlbox@users.noreply.github.com>
2021-03-08 03:42:30 +0900

    no message

--- fm_psg/build.sh
+++ fm_psg/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-for N in `seq -f '%02g' 1 29` ; do
-  mgsc-js -o mgs/ys1ex_${N}.mgs mml/ys1ex_${N}.mml 
-  ../../kss2vgm/build/kss2vgm -p150 -ovgm/ys1ex_${N}.vgm mgs/ys1ex_${N}.mgs
-  gzip -n --stdout vgm/ys1ex_${N}.vgm > vgm/ys1ex_${N}.vgz
-done
+# for N in `seq -f '%02g' 1 29` ; do
+#   mgsc-js -o mgs/ys1ex_${N}.mgs mml/ys1ex_${N}.mml 
+#   ../../kss2vgm/build/kss2vgm -p150 -ovgm/ys1ex_${N}.vgm mgs/ys1ex_${N}.mgs
+#   gzip -n --stdout vgm/ys1ex_${N}.vgm > vgm/ys1ex_${N}.vgz
+# done
 
-for N in `seq -f '%02g' 0 30` ; do
-  mgsc-js -o mgs/ys2ex_${N}.mgs mml/ys2ex_${N}.mml 
-  ../../kss2vgm/build/kss2vgm -p150 -ovgm/ys2ex_${N}.vgm mgs/ys2ex_${N}.mgs
-  gzip -n --stdout vgm/ys2ex_${N}.vgm > vgm/ys2ex_${N}.vgz
-done
+# for N in `seq -f '%02g' 0 30` ; do
+#   mgsc-js -o mgs/ys2ex_${N}.mgs mml/ys2ex_${N}.mml 
+#   ../../kss2vgm/build/kss2vgm -p150 -ovgm/ys2ex_${N}.vgm mgs/ys2ex_${N}.mgs
+#   gzip -n --stdout vgm/ys2ex_${N}.vgm > vgm/ys2ex_${N}.vgz
+# done
 
 for N in 02 17 21 29 ; do
   mgsc-js -o mgs/ys368_${N}.mgs mml/ys368_${N}.mml 
